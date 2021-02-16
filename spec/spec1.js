32f6141f46f55ecf86a2564b5c3f8627bc76f351
AngieD8615 <adavidson1@gmail.com>
2021-02-16 01:55:10 -0700

    completed maxDepth with tests time complexity: O(n)

--- spec/spec1.js
+++ spec/spec1.js
@@ -1,109 +1,162 @@
 var assert = chai.assert;
 
 describe("DefangingAnIPAdress", () => {
-  it('should return a sting', () => {
-    var result = defangIPaddr("1.1.1.1")
-    assert.equal(typeof result, "string")
+  it("should return a sting", () => {
+    var result = defangIPaddr("1.1.1.1");
+    assert.equal(typeof result, "string");
   });
   it('should replace a single "." with "[.]"', () => {
-    var result = defangIPaddr(".")
-    assert.equal(result, "[.]")
+    var result = defangIPaddr(".");
+    assert.equal(result, "[.]");
   });
   it('should replace every "." with "[.]"', () => {
-    var result = defangIPaddr("1.1.1.1")
-    assert.equal(result, "1[.]1[.]1[.]1")
+    var result = defangIPaddr("1.1.1.1");
+    assert.equal(result, "1[.]1[.]1[.]1");
   });
   it('should replace every "." with "[.]"', () => {
-    var result = defangIPaddr("255.100.50.0")
-    assert.equal(result, "255[.]100[.]50[.]0")
+    var result = defangIPaddr("255.100.50.0");
+    assert.equal(result, "255[.]100[.]50[.]0");
   });
   it('if the address does not have a "." it should not change', () => {
-    var address = "255100500"
-    var result = defangIPaddr("255100500")
-    assert.equal(result, address)
+    var address = "255100500";
+    var result = defangIPaddr("255100500");
+    assert.equal(result, address);
   });
 });
 
 describe("Number of Good Pairs", () => {
-  it('should handle an empty array', ()=> {
+  it("should handle an empty array", () => {
     var nums = [];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 0);
-  })
-  it('should handle an array with only 1 element', ()=> {
+  });
+  it("should handle an array with only 1 element", () => {
     var nums = [4];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 0);
-  })
-  it('should handle an array', ()=> {
+  });
+  it("should handle an array", () => {
     var nums = [4, 4];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 1);
-  })
-  it('should handle identical values as the highest value', ()=> {
-    var nums = [1,2,3,4,4,4];
+  });
+  it("should handle identical values as the highest value", () => {
+    var nums = [1, 2, 3, 4, 4, 4];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 3);
-  })
-  it('should handle example 1', ()=> {
-    var nums = [1,2,3,1,1,3];
+  });
+  it("should handle example 1", () => {
+    var nums = [1, 2, 3, 1, 1, 3];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 4);
-  })
-  it('should handle example 1 sorted', ()=> {
-    var nums = [1,1,1,2,3,3];
+  });
+  it("should handle example 1 sorted", () => {
+    var nums = [1, 1, 1, 2, 3, 3];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 4);
-  })
-  it('should handle example 2', ()=> {
-    var nums = [1,1,1,1];
+  });
+  it("should handle example 2", () => {
+    var nums = [1, 1, 1, 1];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 6);
-  })
-  it('should handle when there are no identical pairs', ()=> {
-    var nums = [1,2,3,4];
+  });
+  it("should handle when there are no identical pairs", () => {
+    var nums = [1, 2, 3, 4];
     var result = numIdenticalPairs(nums);
     assert.equal(result, 0);
-  })
-
+  });
 });
 
 describe("Jewels and Stones", () => {
   it("if Jewels or Stones is an empty string, it should return 0", () => {
     const empty = "";
-    const jewels = "af"
-    const stones = "asdfASDF"
+    const jewels = "af";
+    const stones = "asdfASDF";
 
-    let jewelsEmpty = numJewelsInStones(empty, stones)
-    let stonesEmpty = numJewelsInStones(jewels, empty)
-    let bothEmpty = numJewelsInStones(empty, empty)
-    assert.equal(jewelsEmpty, 0)
-    assert.equal(stonesEmpty, 0)
-    assert.equal(bothEmpty, 0)
+    let jewelsEmpty = numJewelsInStones(empty, stones);
+    let stonesEmpty = numJewelsInStones(jewels, empty);
+    let bothEmpty = numJewelsInStones(empty, empty);
+    assert.equal(jewelsEmpty, 0);
+    assert.equal(stonesEmpty, 0);
+    assert.equal(bothEmpty, 0);
   });
   it("should return a number", () => {
-    const jewels = "ab"
-    const stones = "ab"
-    const result = numJewelsInStones(jewels, stones)
-    assert.equal(typeof result, "number")
-    assert.equal(result, 2)
+    const jewels = "ab";
+    const stones = "ab";
+    const result = numJewelsInStones(jewels, stones);
+    assert.equal(typeof result, "number");
+    assert.equal(result, 2);
   });
   it("should return the number of times jewels appears in stones when jewels is a single letter", () => {
-    const jewels = "c"
-    const stones1 = "abcd"
-    const stones2 = "CacbcdC"
-    const stones3 = "acbcdc"
-    const stones4 = "abdCDSA"
-    assert.equal(numJewelsInStones(jewels, stones1), 1)
-    assert.equal(numJewelsInStones(jewels, stones2), 2)
-    assert.equal(numJewelsInStones(jewels, stones3), 3)
-    assert.equal(numJewelsInStones(jewels, stones4), 0)
+    const jewels = "c";
+    const stones1 = "abcd";
+    const stones2 = "CacbcdC";
+    const stones3 = "acbcdc";
+    const stones4 = "abdCDSA";
+    assert.equal(numJewelsInStones(jewels, stones1), 1);
+    assert.equal(numJewelsInStones(jewels, stones2), 2);
+    assert.equal(numJewelsInStones(jewels, stones3), 3);
+    assert.equal(numJewelsInStones(jewels, stones4), 0);
   });
   it("should return the number of times jewels appears in stones", () => {
-    const jewels = "aB"
-    const stones = "ssddaabbBBAASS"
-    const result = numJewelsInStones(jewels, stones)
-    assert.equal(result, 4)
+    const jewels = "aB";
+    const stones = "ssddaabbBBAASS";
+    const result = numJewelsInStones(jewels, stones);
+    assert.equal(result, 4);
   });
+});
 
+describe("Max depth for a Binary Tree", () => {
+  it("should return 0 when there is no root value", () => {
+    let root = null;
+    let result = maxDepth(root);
+    assert.equal(result, 0);
+  });
+  it("should return 1 when the BT only has the root node", () => {
+    let root = { val: 6, left: null, right: null };
+    let result = maxDepth(root);
+    assert.equal(result, 1);
+  });
+  it("should return the maximum depth", () => {
+    let root = {
+      val: 10,
+      left: {
+        val: 1,
+        left: { val: 2, left: null, right: null },
+        right: {
+          val: 3,
+          left: { val: 4, left: null, right: null },
+          right: { val: 5, left: null, right: null },
+        },
+      },
+      right: { val: 6, left: null, right: null }
+    };
+    let result = maxDepth(root);
+    assert.equal(result, 4);
+  });
+  it("should return the maximum depth", () => {
+    let root = {
+      val: 3, 
+      left: {
+        val: 9,
+        left: null, 
+        right: null  
+      },
+      right: {
+        val: 20, 
+        left: {
+          val: 15, 
+          left: null,
+          right: null
+        },
+        right: {
+          val: 7, 
+          left: null,
+          right: null
+        }
+      }
+    };
+    let result = maxDepth(root);
+    assert.equal(result, 3);
+  });
 });
--- src/leetcodeEasy.js
+++ src/leetcodeEasy.js
@@ -90,3 +90,87 @@ var numJewelsInStones = function(jewels, stones) {
   }
   return count;
 };
+
+// Max depth of a binary tree
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest 
+// path from the root node down to the farthest leaf node.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+/* {
+    val: 3, 
+    left: {
+      val: 9,
+      left: null, 
+      right: null  
+    },
+    right: {
+      val: 20, 
+      left: {
+        val: 15, 
+        left: null,
+        right: null
+      },
+      right: {
+        val: 7, 
+        left: null,
+        right: null
+      }
+    }
+  }
+*/
+// Output: 3
+
+// Example 2:
+// Input: root = [1,null,2]
+// Output: 2
+
+// Example 3:
+// Input: root = []
+// Output: 0
+
+// Example 4:
+// Input: root = [0]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
+
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0
+  } else if (root.left === null && root.right === null) {
+    return 1
+  }
+
+  var curCount = 0  
+  var maxCount = 0
+  const helper = (node) => {
+    // base case 
+    if (node.left === null && node.right === null) {
+      curCount++;
+      maxCount = Math.max(curCount, maxCount)
+      curCount--;
+      return
+    } 
+    // non base
+    if (node.left) {
+      curCount++
+      helper(node.left)
+      curCount--
+    }
+    if (node.right) {
+      curCount++
+      helper(node.right)
+      curCount--
+    }
+  }
+  helper(root);
+  return maxCount
+
+};
